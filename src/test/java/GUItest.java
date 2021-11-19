3f68532afeebbde7a89f689bcded38b40addf9e6
Rasmus Thomsen <rasmus0408@gmail.com>
2021-11-19 08:21:50 +0100

    Håber det her virker

--- src/test/java/GUItest.java
+++ src/test/java/GUItest.java
@@ -1,12 +1,14 @@
+import Controllers.GUIcreator;
 import gui_main.GUI;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
 
 class GUItest {
 
     @Test
     void guiTest(){
-        GUI gui = new GUI();
-        gui.showMessage("hey");
+            GUI gui = new GUI(GUIcreator.guiCreator(), Color.GRAY.brighter());
 
 
     }
