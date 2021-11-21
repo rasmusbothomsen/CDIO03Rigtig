13018a5a034307937f2e9a09799c39f2553e32f5
Rasmusbothomsen <rasmus0408@gmai.com>
2021-11-21 21:52:36 +0100

    FAndt ud af det

---
+++ src/main/java/Controllers/StartTester.java
@@ -0,0 +1,11 @@
+package Controllers;
+
+public class StartTester {
+    public static void main(String[] args) {
+        StartOfGameGui startOfGameGui = new StartOfGameGui();
+        System.out.println(startOfGameGui.getLanguage());
+
+
+
+    }
+}
