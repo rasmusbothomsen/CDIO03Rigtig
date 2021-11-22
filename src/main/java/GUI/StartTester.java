bccd9b4b2c753874b1ddc73002c166bd81373aea
Rasmus Thomsen <rasmus0408@gmail.com>
2021-11-22 16:32:54 +0100

    Lavet ScoreBoard færdig

--- src/main/java/GUI/StartTester.java
+++ src/main/java/GUI/StartTester.java
@@ -6,10 +6,21 @@ import gui_main.GUI;
 
 import java.awt.*;
 import java.net.URL;
+import java.util.Scanner;
 
 public class StartTester {
     public static void main(String[] args) {
-        new TextFileReader(".txt");
-        IntroGui introGui = new IntroGui();
+        String[][] test = {{"hey", "heyoo", "hyeoo", "heyy"}, {"yo", "yo", "yo", "yo"}, {"yo", "yo", "yo", "yo"}};
+        String[][] result = new String[4][4];
+        for (int a = 0, i = 0; a < 4; a++, i = 0) {
+
+            result[i][a] = "#" + i;
+            i++;
+            result[i][a] = "hey";
+            i++;
+            result[i][a] = "heyo" + "$";
+            result[i][a] = "heyeyeyey";
+        }
+        new ScoreBoard(test);
     }
 }
--- src/test/java/Controllers/EndOfGameTester.java
+++ src/test/java/Controllers/EndOfGameTester.java
@@ -1,6 +1,7 @@
 package Controllers;
 
 import GUI.GUIHandler;
+import GUI.ScoreBoard;
 import Objects.PlayerCreators;
 import TurnHandling.Player;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,21 @@ class EndOfGameTester {
     new GameController(players);
     players[0].setMoney(0);
     GameController.upDatePlayerBalance();
-    assertNull(GameController.returnIfPlayerBroke());
+    printEndOfGame(players);
+    }
+    private static void printEndOfGame(Player[] players) {
+        String[][] result = new String[players.length][4];
+        for (int a = 0, i = 0; a < players.length; a++, i = 0) {
+
+            result[a][i] = "#" + a+1;
+            i++;
+            result[a][i] = players[a].getName();
+            i++;
+            result[a][i] = players[a].getMoney() + "$";
+            i++;
+            result[a][i] = GameController.getPlayerHouses(players[a]);
+
+        }
+        new ScoreBoard(result);
     }
 }
